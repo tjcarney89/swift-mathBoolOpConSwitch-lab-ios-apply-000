88bdcd3b48d68c53c1c3c40563c81d2046d67891
TJ <tjcarney@stny.rr.com>
2016-10-12 07:41:21 -0400

    Completed Lab and Passed Tests

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -15,7 +15,77 @@ class ViewController: UIViewController {
         
     }
     
-    // Implement your functions here!
+    func averageIsAbove(_ a:Double, _ b:Double, _ c:Double) -> Bool {
+        
+        if (a+b+c)/3.0 > 75 {
+            
+            return true
+            
+        } else {
+            
+            return false
+            
+        }
+        
+    }
+    
+    
+    
+    func passwordCombo(username:String, password:Int) -> String {
+        
+        if username == "Jerry" && password%3==0 {
+            
+            return "Welcome!"
+            
+        } else if username == "Elaine" && password%3==0 {
+            
+            return "Welcome!"
+            
+        } else if username == "Michael" && password%3==0 {
+            
+            return "Welcome!"
+            
+        } else {
+            
+            return "Access Denied"
+            
+        }
+        
+    }
+    
+    
+    
+    func describe(emoji:String) -> String {
+        
+        if emoji=="💋" {
+            
+            return "Kiss"
+            
+        } else if emoji=="🐈" {
+            
+            return "Cat"
+            
+        } else if emoji=="🐢" {
+            
+            return "Turtle"
+            
+        } else if emoji=="🍕" {
+            
+            return "Pizza"
+            
+        } else if emoji=="👻" {
+            
+            return "Ghost"
+            
+        } else {
+            
+            return "Unknown"
+            
+        }
+        
+    }
+    
+
     
 
 }
